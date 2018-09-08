6acd43a0109f7040cd624e1ec1612e5ee2dd0f54
Kirill Novik <jimmynovik@gmail.com>
2018-09-08 15:59:34 +0000

    Initial Commit

---
+++ add_item.py
@@ -0,0 +1,27 @@
+import requests;
+import click;
+import json;
+import pdb;
+
+URL = 'https://personal-dashboard-umbrella-kino6052.c9users.io'
+
+def postItem(title):
+    payload = { 'title': title.encode('utf-8') }
+    item = requests.post('{}/item'.format(URL), json=payload).json()
+    return item
+
+def addItemAsChild(parent_id, child_id):
+    item = requests.put('{}/item/{}/{}'.format(URL, parent_id, child_id)).json()
+    return item
+        
+@click.command()
+@click.option('--id', help='id of a new node')
+@click.option('--title', prompt=True, help='title of the node')
+def add_item(id, title):
+    if id is not None and title is not None:
+        item = postItem(title)
+        item_id = item[u'_id']
+        addItemAsChild(id, item_id)
+        
+if __name__ == '__main__':
+    add_item()
---
+++ print.py
@@ -0,0 +1,82 @@
+import requests;
+import click;
+import json;
+import pdb;
+
+URL = 'https://personal-dashboard-umbrella-kino6052.c9users.io'
+
+def getItem(id):
+    item = requests.get('{}/item/{}'.format(URL, id)).json()
+    return item[0]
+
+def getNotesForItem(id):
+    notes = []
+    item_notes = requests.get('{}/item-note?itemId={}'.format(URL, id)).json()
+    for item_note in item_notes:
+        note_id = item_note[u'noteId']
+        note = requests.get('{}/note/{}'.format(URL, note_id)).json()[0]
+        notes.append(note)
+    return notes
+    
+def printNotesForItem(id):
+    item_notes = requests.get('{}/item-note?itemId={}'.format(URL, id)).json()
+    for item_note in item_notes:
+        note_id = item_note[u'noteId']
+        note = requests.get('{}/note/{}'.format(URL, note_id)).json()[0]
+        click.secho(note[u'title'])
+
+def getNode(id_tuple):
+    id = id_tuple[0]
+    indent = id_tuple[1]
+    json_obj = getItem(id)
+    title = json_obj[u'title'].encode('utf-8')
+    # pdb.set_trace()
+    click.secho('{}'.format('|\t'*indent + '|_'), nl=False)
+    click.secho(' {} '.format(title), fg='red', bg='white', nl=False)
+    click.secho(' {} '.format(id), fg='white')
+    return json_obj
+    
+def getChildren(json_node, indent):
+    children = []
+    result = []
+    # pdb.set_trace()
+    if u'children' in json_node:
+        children = json_node[u'children']
+    for child in children:
+        child = child.encode('utf-8')
+        result.append((child, indent))
+    return result
+    
+def print_tree_with_depth(id, depth, notes):
+    stack = [(id, 0)]
+    while True:
+        try:
+            if len(stack) == 0:
+                break;
+            cur_id = stack.pop()
+            indent = cur_id[1]
+            if indent >= depth:
+                continue
+            json_node = getNode(cur_id);
+            if notes:
+                printNotesForItem(cur_id[0])
+            children = getChildren(json_node, indent + 1);
+            stack = stack + children
+        except Exception:
+            print 'exception';
+            continue;
+        
+        
+@click.command()
+@click.option('--id', default='5b6605a886ec2e1a5a713867', help='id of a Node')
+@click.option('--title', help='Title to search')
+@click.option('--depth', default=0, help='Depth of the tree to show')
+@click.option('--notes', is_flag=True)
+def print_tree(id, title, depth, notes):
+    if depth > 0 and depth <= 5:
+        print_tree_with_depth(id, depth, notes)
+
+if __name__ == '__main__':
+    print_tree()
+    
+# 5b6605a886ec2e1a5a713867
